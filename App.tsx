117d83dba102f81c041128b0dd4065dc0fefcaa6
llama <llama@llamas-MacBook-Pro.local>
2024-09-09 21:44:46 +0545

    Feat: first commit

---
+++ App.tsx
@@ -0,0 +1,10 @@
+import { Text, View } from 'react-native';
+import Login from './screens/Login';
+
+export default function App() {
+  return (
+    <View>
+      <Login />
+    </View>
+  );
+}
---
+++ screens/Home/index.tsx
@@ -0,0 +1,12 @@
+
+
+import React from 'react';
+import { SafeAreaView, Text } from 'react-native';
+
+export default function Home() {
+  return (
+    <SafeAreaView>
+        <Text>Home page</Text>
+    </SafeAreaView>
+  )
+}
